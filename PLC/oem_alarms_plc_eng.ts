44d91e97d549f6e87367e18e4edca16955b000ca
Marco Nie <github@niekun.net>
2020-04-15 20:56:47 +0800

    add plc

---
+++ PLC/oem_alarms_plc_eng.ts
@@ -0,0 +1,306 @@
+﻿<!DOCTYPE TS []>
+<TS>
+	<context>
+		<name>slaeconv</name>
+		<message>
+                <source>700000/PLC/828D</source>
+		<translation>Emergency stop, please check SB1!</translation>
+		</message>
+		<message>
+		<source>700001/PLC/828D</source>
+		<translation>Overload of motor for grinding wheel </translation>
+		</message>
+		<message>
+		<source>700002/PLC/828D</source>
+		<translation>Overload of motor for dressing wheel </translation>
+		</message>
+		<message>
+		<source>700003/PLC/828D</source>
+		<translation>Overload of motor for hydraulic pump </translation>
+		</message>
+		<message>
+		<source>700004/PLC/828D</source>
+		<translation> Overload of motor for hydrostatic pump </translation>
+		</message>
+		<message>
+		<source>700005/PLC/828D</source>
+		<translation> Overload of intermittent lubrication pump </translation>
+		</message>
+		<message>
+		<source>700006/PLC/828D</source>
+		<translation> Overload of coolant pump for grinding </translation>
+		</message>
+		<message>
+		<source>700007/PLC/828D</source>
+		<translation> Overload of coolant pump for dressing </translation>
+		</message>
+		<message>
+		<source>700008/PLC/828D</source>
+		<translation> Overload of coolant pump for showering </translation>
+		</message>
+		<message>
+		<source>700009/PLC/828D</source>
+		<translation></translation>
+		</message>
+		<message>
+		<source>700010/PLC/828D</source>
+		<translation> Overload of coolant tank </translation>
+		</message>
+		<message>
+		<source>700011/PLC/828D</source>
+		<translation> Overload of wheel motor for external grinding </translation>
+		</message>
+		<message>
+		<source>700012/PLC/828D</source>
+		<translation></translation>
+		</message>
+		<message>
+		<source>700013/PLC/828D</source>
+		<translation> Overload of hydrostatic oil cooler </translation>
+		</message>
+		<message>
+		<source>700014/PLC/828D</source>
+		<translation> Overload of water cooler on spindle </translation>
+		</message>
+		<message>
+		<source>700015/PLC/828D</source>
+		<translation> Overload of coolant pump on spindle </translation>
+		</message>
+		<message>
+		<source>700016/PLC/828D</source>
+		<translation> Overload of lubrication pump on headstock </translation>
+		</message>
+		<message>
+		<source>700017/PLC/828D</source>
+		<translation> Overload of intermittent lubrication pump 2 </translation>
+		</message>
+		<message>
+		<source>700018/PLC/828D</source>
+		<translation>Overload of milling spindle</translation>
+		</message>
+		<message>
+		<source>700019/PLC/828D</source>
+		<translation>Overload of control transformer</translation>
+		</message>
+		<message>
+		<source>700020/PLC/828D</source>
+		<translation></translation>
+		</message>
+		<message>
+		<source>700021/PLC/828D</source>
+		<translation>Overload of wheel motor for internal grinding</translation>
+		</message>
+		<message>
+		<source>700022/PLC/828D</source>
+		<translation>Overload of driving power </translation>
+		</message>
+		<message>
+		<source>700023/PLC/828D</source>
+		<translation>Motor overload for grinding wheel 2 </translation>
+		</message>
+		<message>
+		<source>700024/PLC/828D</source>
+		<translation>Converter alarm for grinding wheel 2 </translation>
+		</message>
+		<message>
+		<source>700025/PLC/828D</source>
+		<translation></translation>
+		</message>
+		<message>
+		<source>700026/PLC/828D</source>
+		<translation> Converter alarm for grinding wheel </translation>
+		</message>
+		<message>
+		<source>700027/PLC/828D</source>
+		<translation>Temperature alarm for electric spindle </translation>
+		</message>
+		<message>
+		<source>700028/PLC/828D</source>
+		<translation>Driver alarm for dressing wheel</translation>
+		</message>
+		<message>
+		<source>700029/PLC/828D</source>
+		<translation>Temperature alarm for driver</translation>
+		</message>
+		<message>
+		<source>700030/PLC/828D</source>
+		<translation>Not ready for driver </translation>
+		</message>
+		<message>
+		<source>700031/PLC/828D</source>
+		<translation>Alarm for cutter spindle </translation>
+		</message>
+		<message>
+		<source>700032/PLC/828D</source>
+		<translation>Alarm for hydraulic oil cooler </translation>
+		</message>
+		<message>
+		<source>700033/PLC/828D</source>
+		<translation>Alarm for water cooling device</translation>
+		</message>
+		<message>
+		<source>700034/PLC/828D</source>
+		<translation> Alarm for coolant oil tank </translation>
+		</message>
+		<message>
+		<source>700035/PLC/828D</source>
+		<translation> Alarm for lower pressure </translation>
+		</message>
+		<message>
+		<source>700036/PLC/828D</source>
+		<translation> Alarm for higher pressure </translation>
+		</message>
+		<message>
+		<source>700037/PLC/828D</source>
+		<translation>Converter alarm for internal grinding wheel </translation>
+		</message>
+		<message>
+		<source>700038/PLC/828D</source>
+		<translation> Converter alarm for external grinding wheel </translation>
+		</message>
+		<message>
+		<source>700039/PLC/828D</source>
+		<translation>Alarm for abnormal pressure</translation>
+		</message>
+		<message>
+		<source>700040/PLC/828D</source>
+		<translation>Fault on asynchronous subroutine 1 </translation>
+		</message>
+		<message>
+		<source>700041/PLC/828D</source>
+		<translation>Have not checked the cooling water on spindle!</translation>
+		</message>
+		<message>
+		<source>700042/PLC/828D</source>
+		<translation> Have not checked the oil lubrication on E-spindle!</translation>
+		</message>
+		<message>
+		<source>700043/PLC/828D</source>
+		<translation> Have not checked the lubrication on headstock!</translation>
+		</message>
+		<message>
+		<source>700044/PLC/828D</source>
+		<translation>Lower hydraulic pressure </translation>
+		</message>
+		<message>
+		<source>700045/PLC/828D</source>
+		<translation>Lower hydrostatic pressure</translation>
+		</message>
+		<message>
+		<source>700046/PLC/828D</source>
+		<translation></translation>
+		</message>
+		<message>
+		<source>700047/PLC/828D</source>
+		<translation>Temperature alarm for motor on dressing wheel</translation>
+		</message>
+		<message>
+		<source>700048/PLC/828D</source>
+		<translation>Not enabled for driver, please enabled !</translation>
+		</message>
+		<message>
+		<source>700049/PLC/828D</source>
+		<translation>Attention, the present override is zero!</translation>
+		</message>
+		<message>
+		<source>700050/PLC/828D</source>
+		<translation>Start hydraulic pump please! </translation>
+		</message>
+		<message>
+		<source>700051/PLC/828D</source>
+		<translation> Start lubrication pump , please!</translation>
+		</message>
+		<message>
+		<source>700052/PLC/828D</source>
+		<translation>Chuck is in loosen status!</translation>
+		</message>
+		<message>
+		<source>700053/PLC/828D</source>
+		<translation>Attention, the probe is stretching!</translation>
+		</message>
+		<message>
+		<source>700054/PLC/828D</source>
+		<translation>The clamping device is in loosen status.</translation>
+		</message>
+		<message>
+		<source>700055/PLC/828D</source>
+		<translation>Do not start program because the safety door is not closed!</translation>
+		</message>
+		<message>
+		<source>700056/PLC/828D</source>
+		<translation>The grinding wheel has not reached the reset speed !</translation>
+		</message>
+		<message>
+		<source>700057/PLC/828D</source>
+		<translation> The dressing wheel has not reached the reset speed !</translation>
+		</message>
+		<message>
+		<source>700058/PLC/828D</source>
+		<translation>The swing cylinder for dressing has not return to its initial position !</translation>
+		</message>
+		<message>
+		<source>700059/PLC/828D</source>
+		<translation>Overload of oil mist exhaustor </translation>
+		</message>
+		<message>
+		<source>700060/PLC/828D</source>
+		    <translation>DRF function is valid, manual intervention X1 status, pay more attention to MPG!</translation>
+		</message>
+		<message>
+		<source>700061/PLC/828D</source>
+		<translation> DRF function is valid, manual intervention X10 status, pay more attention to MPG!</translation>
+		</message>
+		<message>
+		<source>700062/PLC/828D</source>
+		<translation> DRF function is valid, manual intervention X100 status, pay more attention to MPG!</translation>
+		</message>
+		<message>
+		<source>700063/PLC/828D</source>
+		<translation>Make each axis return to its reference point!</translation>
+		</message>
+		<message>
+		<source>700064/PLC/828D</source>
+		<translation>Overload of chip removal</translation>
+		</message>
+		<message>
+		<source>700065/PLC/828D</source>
+		<translation>Overload of air-conditioner on E-cabinet</translation>
+		</message>
+		<message>
+		<source>700066/PLC/828D</source>
+		<translation> Alarm of air-conditioner on E-cabinet </translation>
+		</message>
+		<message>
+		<source>700067/PLC/828D</source>
+		<translation> Overload of magnetic separator </translation>
+		</message>
+		<message>
+		<source>700068/PLC/828D</source>
+		<translation>Until the axis X return to its zero position, the axis Z can return!</translation>
+		</message>
+		<message>
+		<source>700069/PLC/828D</source>
+		<translation>Please check the speed of grinding wheel correctly, because the speed for spindle 1/2 has been reached the rated limit speed！</translation>
+		</message>
+		<message>
+		<source>700070/PLC/828D</source>
+		<translation>Overload of motor for washing pump</translation>
+		</message>
+		<message>
+		<source>700071/PLC/828D</source>
+		<translation>Check for clamping pressure of axis A </translation>
+		</message>
+		<message>
+		<source>700072/PLC/828D</source>
+		<translation>Please add lubricate because the liquid level of intermittent lubrication pump  is too low !</translation>
+		</message>
+		<message>
+		<source>700073/PLC/828D</source>
+		<translation> Please add lubricate because the liquid level of intermittent lubrication pump 2 is too low !</translation>
+		</message>
+		<message>
+		<source>700074/PLC/828D</source>
+		<translation>The date storage is on…</translation>
+		</message>
+        </context>
+        </TS>
